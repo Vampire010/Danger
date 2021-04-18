748ca33ba77a8ce736178e81b2c303dd12598a8e
girishrathode.rathode@gmail.com <girishrathode.rathode@gmail.com>
2021-04-18 09:41:44 +0530

    18 - 05 - 2021

---
+++ Sunny_Java_Project/src/Collection_MAP/getOrDefault_ex1.java
@@ -0,0 +1,31 @@
+package Collection_MAP;
+
+import java.util.WeakHashMap;
+
+public class getOrDefault_ex1 {
+
+	public static void main(String[] args)
+	{
+		// Creating WeakHashMap of even numbers
+		WeakHashMap<String, Integer> numbers = new WeakHashMap<>();
+		
+		String one = new String("One");
+		Integer oneValue = 1;
+		numbers.put(one, oneValue);
+		
+		String two = new String("Two");
+		Integer twoValue = 2;
+		numbers.put(two, twoValue);
+	
+		System.out.println("WeakHashMap: " + numbers);
+		
+		// Using get()
+		int value1 = numbers.get("Two");
+		System.out.println("Using get(): " + value1);
+		
+		// Using getOrDefault()
+		int value2 = numbers.getOrDefault("Four", 4);
+		System.out.println("Using getOrDefault(): " + value2);
+	}
+
+}
